7b5ebc7218d5680f9f29f0ebb88d528e6403f42c
TheLastGimbus <mateusz.soszynski@tuta.io>
2025-12-02 22:40:36 +0100

    replace with dart 🎯

---
+++ projekt3/dartt/bin/dartt.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:server_nano/server_nano.dart';
+
+Function(String message)? send;
+
+void main(List<String> arguments) async {
+  final server = Server();
+
+  server.get('/', (req, res) {
+    res.sendHtmlText("<h1>Dupala!</h1>");
+  });
+
+  server.ws('/ws', (socket) {
+    socket.onMessage((msg) async {
+      print(msg);
+      if (msg is String) {
+        try {
+          final proc = await Process.start('fish', ['-c', msg]);
+          proc.stdout.map(utf8.decode).listen(send);
+          proc.stderr.map(utf8.decode).listen(send);
+        } catch (e) {
+          socket.sendToAll(e.toString());
+        }
+      }
+    });
+    send = socket.sendToAll;
+  });
+
+  await server.listen(host: '0.0.0.0', port: 5000, wsPort: 5001);
+}
---
+++ projekt3/dartt/test/dartt_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartt/dartt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
