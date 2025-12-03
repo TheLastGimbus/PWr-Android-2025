106ab527b4752e247cbfa85b85ff70f914a1b635
TheLastGimbus <mateusz.soszynski@tuta.io>
2025-12-03 08:54:07 +0100

    some extra stuff

--- projekt3/dartt/bin/dartt.dart
+++ projekt3/dartt/bin/dartt.dart
@@ -4,10 +4,9 @@ import 'dart:io';
 
 import 'package:server_nano/server_nano.dart';
 
-Function(String message)? send;
-
 void main(List<String> arguments) async {
   final server = Server();
+  StreamSubscription? ss;
 
   server.get('/', (req, res) {
     res.sendHtmlText("<h1>Dupala!</h1>");
@@ -19,23 +18,26 @@ void main(List<String> arguments) async {
       if (msg is String) {
         try {
           final proc = await Process.start('fish', ['-c', msg]);
-          proc.stdout.map(utf8.decode).listen(send);
-          proc.stderr.map(utf8.decode).listen(send);
+          proc.stdout.map(utf8.decode).listen(socket.sendToAll);
+          proc.stderr.map(utf8.decode).listen(socket.sendToAll);
         } catch (e) {
           socket.sendToAll(e.toString());
         }
       }
     });
-    Stream.periodic(
-      Duration(seconds: 1),
-      (i) => DateTime.now().toIso8601String(),
-    ).listen(socket.sendToAll);
-    send = socket.sendToAll;
+    // ss?.cancel();
+    // ss = Stream.periodic(
+    //   Duration(seconds: 1),
+    //   (i) => DateTime.now().toIso8601String(),
+    // ).listen(socket.sendToAll);
   });
 
-  for (var value in (await NetworkInterface.list(type: InternetAddressType.IPv4))) {
-    print("${value.addresses.first.address} - ${value.name}");
-  };
+  for (var value in (await NetworkInterface.list(
+    type: InternetAddressType.IPv4,
+  ))) {
+    print("${value.addresses.first.address}\n${value.name}");
+  }
+  ;
   print("Port WS: 5001");
 
   await server.listen(host: '0.0.0.0', port: 5000, wsPort: 5001);
